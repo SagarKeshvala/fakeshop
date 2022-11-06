8af5633cd851bcab0d00796d40487ac1468f61cf
Sarman Keshvala <sagarkeshwala15@gmail.com>
2022-11-06 10:42:33 +0530

    added useSelector hook to fetch state data

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Product = () => {
+    const products = useSelector((state) => state.allProducts.products);
+    console.log(products)
+    const {id, title} = products[0];    
     return (
-        <div>
-            <h1>Product</h1>
+        <div className='four column wide'>
+            <div className='ui link cards'>
+                <div className='card'>
+                    <div className='image'>
+
+                    </div>
+                    <div className='content'>
+                        <div className='header'>
+                            {title}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/ProductsListing.js
+++ src/components/ProductsListing.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Product from './Product';
 
 const ProductListing = () => {
+
+    const products = useSelector((state) => state);
+
     return (
-        <div>
-            <h1>Product Listing</h1>
+        <div className='ui grid container'>
+            <Product/>
         </div>
     )
 }

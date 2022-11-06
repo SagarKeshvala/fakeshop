d9acb4cdc9cf0f672158e5141583aa61b09b0a6b
Sarman Keshvala <sagarkeshwala15@gmail.com>
2022-11-06 11:38:16 +0530

    listed products

--- src/components/ProductsListing.js
+++ src/components/ProductsListing.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setProducts } from "../redux/actions/productActions";
 import Product from "./Product";
 
 const ProductListing = () => {
-	const products = useSelector((state) => state);
 	const dispatch = useDispatch();
 
-	const fetchProducts = async () => { 
+	const fetchProducts = async () => {
 		const response = await axios
 			.get("https://fakestoreapi.com/products")
 			.catch((err) => console.log(err));
@@ -17,9 +16,7 @@ const ProductListing = () => {
 
 	useEffect(() => {
 		fetchProducts();
-	}, []);
-
-	console.log(products);
+	});
 
 	return (
 		<div className="ui grid container">
